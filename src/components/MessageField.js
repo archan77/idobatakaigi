a9ce18106dade553e947c6fc9cbcb8cdac08414c
片山 朝子 <archan1@KATAYAMA-no-MacBook-Pro.local>
2021-02-26 16:07:06 +0900

    adjust focusing

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -3,11 +3,12 @@ import { TextField } from '@material-ui/core';
 
 import { pushMessage }  from '../firebase';
 
-const MessageField = ({name, setText, text}) => {
+const MessageField = ({inputEl,name, setText, text}) => {
     const [isComposed, setIsComposed] = useState(false);
 
     return (
         <TextField 
+        autoFocus
         fullWidth={true} 
         onChange={(e) => setText(e.target.value)}
         onKeyDown={(e) => {
